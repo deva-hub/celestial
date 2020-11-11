2a3d8cf263787ed88068cbea3104ffa5d80355f7
Shikanime Deva <deva.shikanime@protonmail.com>
2020-11-11 23:24:44 +0100

    Reword celestial world description

--- apps/celestial_world/lib/celestial_world.ex
+++ apps/celestial_world/lib/celestial_world.ex
@@ -1,9 +1,5 @@
 defmodule CelestialWorld do
   @moduledoc """
-  CelestialWorld keeps the contexts that define your domain
-  and business logic.
-
-  Contexts are also responsible for managing your data, regardless
-  if it comes from the database, an external API or others.
+  The entrypoint for the game server interface.
   """
 end
